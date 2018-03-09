699c6aa0c68fd3b209904cc0cdda7b95a54c986c
awei.cao <awei.cao@cgtz.com>
2018-03-09 16:04:33 +0800

    --更新

---
+++ data-structure/algorithm/classify/classification.py
@@ -0,0 +1,14 @@
+
+class ClassifyAlgorithm:
+    """
+    分类算法接口,子类需实现
+    classify(tuple) 方法:
+    """
+
+    def classify(self, data):
+        """
+        分类算法
+        :param data: 待分类数据
+        :return: 返回数据所属分类
+        """
+        pass
---
+++ data-structure/algorithm/classify/nb.py
@@ -0,0 +1,20 @@
+
+import algorithm.classify.classification as __ai_classify
+import numpy as np
+
+
+class NBClassifyAlgorithm(__ai_classify.ClassifyAlgorithm):
+
+    """
+    朴素贝叶斯分类算法(NB)
+    """
+    def __init__(self):
+        print("朴素贝叶斯分类算法")
+
+    def classify(self, data):
+        print("朴素贝叶斯分类算法")
+
+
+
+
+
